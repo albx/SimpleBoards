b5fd7c8509baed0045f245bb18b38b0caa8f043b
Alberto Mori <albi.mo87@hotmail.it>
2021-09-23 19:16:42 +0200

    Setup bunit. writing some tests

--- src/SimpleBoards.Web.App/Http/BoardsHttpClient.cs
+++ src/SimpleBoards.Web.App/Http/BoardsHttpClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -16,7 +17,14 @@ namespace SimpleBoards.Web.App.Http
 
         public Task<BoardListModel> GetBoardsList() => Http.GetFromJsonAsync<BoardListModel>("api/boards");
 
-        public Task CreateNewBoard(BoardModel model) => Http.PostAsJsonAsync("api/boards", model);
+        public async Task CreateNewBoard(BoardModel model)
+        {
+            var response = await Http.PostAsJsonAsync("api/boards", model);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"An error occured. Status code {response.StatusCode}, Content: {await response.Content.ReadAsStringAsync()}");
+            }
+        }
 
         public Task<BoardModel> GetBoardDetail(int boardId) => Http.GetFromJsonAsync<BoardModel>($"api/boards/{boardId}");
     }
---
+++ tests/SimpleBoards.Web.App.Test/Http/Mocks/HttpClientMockExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using RichardSzalay.MockHttp;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace SimpleBoards.Web.App.Test.Http.Mocks
+{
+    public static class HttpClientMockExtensions
+    {
+        public static MockHttpMessageHandler AddHttpClientMock(this TestServiceProvider services)
+        {
+            var httpHandler = new MockHttpMessageHandler();
+            var client = httpHandler.ToHttpClient();
+            client.BaseAddress = new Uri("https://localhost");
+
+            services.AddSingleton(client);
+            return httpHandler;
+        }
+
+        public static MockedRequest RespondAsJson<T>(this MockedRequest request, T content)
+        {
+            request.Respond(r =>
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                response.Content = new StringContent(JsonSerializer.Serialize(content));
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                return response;
+            });
+
+            return request;
+        }
+
+        public static MockedRequest RespondWithStatusCode(this MockedRequest request, HttpStatusCode statusCode)
+        {
+            request.Respond(r =>
+            {
+                var response = new HttpResponseMessage(statusCode);
+                return response;
+            });
+
+            return request;
+        }
+    }
+}
